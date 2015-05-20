cf17701d8b48c242a953036e866015332c95680f
Gobbyy <max.mauray@gmail.com>
2015-05-20 11:49:28 +0200

    header 2 levels

--- drupal/sites/all/themes/bootstrap/theme/system/page.tpl.php
+++ drupal/sites/all/themes/bootstrap/theme/system/page.tpl.php
@@ -98,15 +98,23 @@
     <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
       <div class="navbar-collapse collapse">
         <nav role="navigation">
-          <?php if (!empty($primary_nav)): ?>
-            <?php print render($primary_nav); ?>
-          <?php endif; ?>
+			<div class="span10">
+			<div class="span2 offset1">
+		  <?php if (!empty($page['navigation'])): ?>
+            <?php print render($page['navigation']); ?> 
+		<?php endif; ?>
           <?php if (!empty($secondary_nav)): ?>
             <?php print render($secondary_nav); ?>
           <?php endif; ?>
-          <?php if (!empty($page['navigation'])): ?>
-            <?php print render($page['navigation']); ?>
+		  		</div>
+	 		 </div>			
+			<div class="span10">
+          <?php if (!empty($primary_nav)): ?>
+            <?php print render($primary_nav); ?>
           <?php endif; ?>
+	  		</div>
+			
+			
         </nav>
       </div>
     <?php endif; ?>
