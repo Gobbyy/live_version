4c1334511675620a59facfa8a5ec9bfd3dc3fc60
Gobbyy <max.mauray@gmail.com>
2015-05-20 16:00:01 +0200

    update

--- drupal/sites/all/themes/bootstrap/theme/system/page.tpl.php
+++ drupal/sites/all/themes/bootstrap/theme/system/page.tpl.php
@@ -75,12 +75,15 @@
 ?>
 <header id="navbar" role="banner" class="<?php print $navbar_classes; ?>">
   <div class="container">
+	  <div class="col-md-2">
     <div class="navbar-header">
+		
       <?php if ($logo): ?>
       <a class="logo navbar-btn pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
         <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
       </a>
       <?php endif; ?>
+	  
 
       <?php if (!empty($site_name)): ?>
       <a class="name navbar-brand" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>"><?php print $site_name; ?></a>
@@ -93,28 +96,35 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
+	  	</div>
     </div>
 
     <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
+
       <div class="navbar-collapse collapse">
         <nav role="navigation">
-			<div class="span10">
-			<div class="span2 offset1">
-		  <?php if (!empty($page['navigation'])): ?>
-            <?php print render($page['navigation']); ?> 
-		<?php endif; ?>
-          <?php if (!empty($secondary_nav)): ?>
-            <?php print render($secondary_nav); ?>
-          <?php endif; ?>
-		  		</div>
-	 		 </div>			
-			<div class="span10">
-          <?php if (!empty($primary_nav)): ?>
-            <?php print render($primary_nav); ?>
-          <?php endif; ?>
-	  		</div>
-			
-			
+    			<div id="nav-haut" class="col-md-10">
+/* barre de recherche */
+    		      <?php if (!empty($page['navigation'])): ?>
+                  <?php print render($page['navigation']); ?> 
+    		      <?php endif; ?>
+/* nav2 qui est le menu du haut */
+              <?php if (!empty($secondary_nav)): ?>
+                <?php print render($secondary_nav); ?>
+              <?php endif; ?>
+
+    	 		</div>		
+
+    			<div id="nav-bas" class="col-md-12">
+    				
+/* menu principal en bas */
+              <?php if (!empty($primary_nav)): ?>
+				  <div class="pull-right">
+                <?php print render($primary_nav); ?>
+              <?php endif; ?>
+
+    	  		</div>
+    	  	</div>		
         </nav>
       </div>
     <?php endif; ?>
